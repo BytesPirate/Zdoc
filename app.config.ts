84ad5b8967903816cc70e140a46b5462c9d6b3c4
Jin Hang <jinhang@mail.14bytes.com>
2023-07-08 22:02:02 +0800

    fix(day1):优化结构

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,36 @@ export default defineAppConfig({
   docus: {
     title: 'Zorro\'s Docs',
     description: '运维笔记',
-    url: 'http://docs.14bytes.com'
+    url: 'http://docs.14bytes.com',
+    socials: {
+      github: 'iA10N3/Zdoc',
+    },
+    footer: {
+      credits: {
+        icon: 'IconDocus',
+        text: 'Powered by Docus',
+        href: 'https://docus.dev',
+      },
+      textLinks: [
+        {
+          text: 'Nuxt',
+          href: 'https://nuxt.com',
+          target: '_blank',
+          rel: 'noopener'
+        }
+      ],
+      iconLinks: [
+        {
+          label: 'NuxtJS',
+          href: 'https://nuxtjs.org',
+          component: 'IconNuxtLabs',
+        },
+        {
+          label: 'Vue Telescope',
+          href: 'https://vuetelescope.com',
+          component: 'IconVueTelescope',
+        },
+      ],
+    },
   }
 })
